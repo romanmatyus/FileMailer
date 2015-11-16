a395f3b583dc406b85f2f8843d883793bfccb30e
Adam Stipak <adam.stipak@diagnose.me>
2015-11-16 10:03:01 +0100

    Add file extenstion to mail file.

--- FileMailer.php
+++ FileMailer.php
@@ -28,6 +28,8 @@ use Nette\Object,
  */
 class FileMailer extends Object implements IMailer
 {
+	const FILE_EXTENSION = 'eml';
+
 	/** @var string */
 	public $tempDir;
 
@@ -51,7 +53,7 @@ class FileMailer extends Object implements IMailer
 
 		preg_match('/Message-ID: <(?<message_id>\w+)[^>]+>/', $content, $matches);
 
-		$path = $this->tempDir."/".$this->prefix.$matches['message_id'];
+		$path = $this->tempDir."/".$this->prefix.$matches['message_id'] . '.' . self::FILE_EXTENSION;
 		if ($bytes = file_put_contents($path, $content))
 			return $bytes;
 		else
